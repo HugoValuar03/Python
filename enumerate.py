d20db065e78de558a1e70241eede72c1f7621c06
Hugo Valuar Bailona <142545466+HugoValuar03@users.noreply.github.com>
2024-01-08 16:07:14 -0300

    Delete enumerate.py

--- enumerate.py
+++
@@ -1,9 +0,0 @@
-lista = ["abacate", "bola", "cachorro"]
-
-print("Modo normal")
-for i in lista:
-    print(i)
-    
-print("Usando a função enumarate")
-for i, nome in enumerate(lista):
-    print(i, nome)
