f7cf767d9754d130774ddcc2fc4f77aeff6dc4a8
Hugo Valuar Bailona <hugovaluarb03@gmail.com>
2024-05-20 02:36:23 -0300

    modulo de functions

---
+++ functions.py
@@ -0,0 +1,10 @@
+### Funções ###
+
+def my_function ():
+    print("Isso é uma função")
+
+def sum_two_values(first_number : int, second_number : int):
+    print(first_number + second_number)
+
+def sum_two_values_with_return(firt_number, second_number): 
+    return firt_number + second_number
